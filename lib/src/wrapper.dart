7241c69f6c06c8a58a70d44ee1abc5e917e34d6e
Ahmed Masoud <maxzod66@gmail.com>
2021-09-26 20:53:37 +0200

    use `https` instead of `http` fix #1

--- lib/src/wrapper.dart
+++ lib/src/wrapper.dart
@@ -5,7 +5,7 @@ import 'package:alquran_cloud/src/exception.dart';
 Future<dynamic> get(String url, {Map<String, dynamic>? query}) async {
   try {
     final _dio = Dio(BaseOptions(
-      baseUrl: 'http://api.alquran.cloud/v1',
+      baseUrl: 'https://api.alquran.cloud/v1',
       followRedirects: true,
       validateStatus: (statusCode) => statusCode != null && statusCode == 200,
     ));
